802f866c496af26fd11f3648d9482fb50d4e6073
natagoh <gohnatasha@gmail.com>
2019-06-23 17:59:05 -0700

    added vue integration

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img src="./assets/ace_clubs.svg"> -->
+    <Card value="ace" suit="clubs"/>
+
+    <!-- your hand -->
+    <!-- <div class="hand" v-for="card in cards">
+      <p> hi </p>
+      <img src="./assets/ace_clubs.svg">
+    </div> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
 
 export default {
   name: 'app',
+  data: function() {
+    return  {
+      cards: ["HI", "JI"]
+    }
+  },
+
   components: {
-    HelloWorld
-  }
+    Card
+  },
+  
+  // created: function () {
+  //  this.url = "../static/" + this.value + "_" + this.suit + ".svg";
+  //   console.log('card value is: ' + src);
+  // }
 }
 </script>
 
@@ -22,7 +40,12 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /*color: #2c3e50;*/
+/*  margin-top: 60px;
+*/
+  position: absolute;
+  top: 0;
+
+  z-index: 1;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card">
+  	<img class="card-img" :src="getImg(value, suit)"> 
+  	<!-- <img class="card-img" src="../../static/ace_clubs.svg">  -->
+  	{{ value }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card', 
+  props: {
+    value: String,
+    suit: String
+  },
+  // computed: {
+  //   url: function() {
+  //     return "../../static/" + this.value + "_" + this.suit + ".svg";
+  //   }
+  // },
+  methods: {
+		getImg(value, suit) {
+		var images = require.context('../assets', false, /\.svg$/)
+    // return images('./' + pet + ".png")
+		return images('./' + this.value + "_" + this.suit + ".svg");
+	}
+}
+  // mounted: function () {
+  // 	this.url = "../static/" + this.value + "_" + this.suit + ".svg";
+  //   console.log('card value is: ' + src);
+  // }
+}
+</script>
+
+<style scoped>
+.card {
+	display: block;
+	float: left;
+  	/*color: red;
+  	font-size: 2em;*/
+}
+</style>
